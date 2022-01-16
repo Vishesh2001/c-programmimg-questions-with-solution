1cc60b839e86a6ffefe17498e3b8e75bed72ab16
VISHESH KUMAR <58973489+Vishesh2001@users.noreply.github.com>
2022-01-16 14:14:27 +0530

    Add files via upload
    this program shows the basic input output in C++.
    iostream is the required header file for input output function.

---
+++ 2_2inputoutput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    short int rahul,dad,mom;
+    cout<<"money by dad";
+    cin>>dad;
+cout<<"money by mom";
+    cin>>mom;
+    rahul=dad+mom;
+cout<<"total money in hand of rahul"<<rahul<<endl;
+    
+
+    return 0;
+}
