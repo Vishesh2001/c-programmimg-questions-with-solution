ec3ac66bf8180f99a66ceda5e6c61b255cdd4904
VISHESH KUMAR <58973489+Vishesh2001@users.noreply.github.com>
2022-01-17 11:40:53 +0530

    Add files via upload
    C++ provides conditional statements.
    one such is "if" statement.
    syntax of "if" statement-
    if(condition =true)
    {
    body.
    }
    another conditional statement is "if else" statement.
    If the condition specified in the if( ) is true, then the body of the "if" is executed , otherwise the body of "else" is executed.
    syntax-
    if(condition=true)
    {
     if body
    }
    else
    {
    else  body
    }.
    there is a concept of nested if-else statements.
    in this concept, multiple if- else conditions can be used inside one another.
    other conditional statement provided by C++ is "switch case " statement.(discussed in the next file.)

---
+++ 2_3ifelse_statement.cpp
@@ -0,0 +1,60 @@
+/*program to apply if else statement and nested if else statement.
+ situtation - 1.take age and check the opportunities.
+              2.maximum of three numbers using if else .
+              3. number is even or odd*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  /*  int age;
+    cout<<"enter your age: ";
+    cin>>age;
+    if(age<13)
+    {
+        cout<<"you are a young boy , do study\n";
+    }        
+    else if(age<20)
+    {
+        cout<<"you are a teenager\n";
+        if(age>17)
+        cout<<"you are eligible for voting according to consitution of india.\n";
+        else
+        cout<<"you are not elsigible for voting.\n";
+    }
+    else{
+        cout<<"you are adult\n";
+        if(age>21)
+        {cout<<"you are eligible for marriage\n";}
+        else cout<<"you r not eligible for marriage\n";
+    }*/
+    
+
+
+    /*short int a,b,c,max;
+    cout<<"\nenter the value of 1st number a=: ";
+    cin>>a;
+    cout<<"\nenter the value of 2nd number b=: ";
+    cin>>b;
+    cout<<"\nenter the value of 3rd number c=: ";
+    cin>>c;
+    max=a;
+    if (b>max)
+    {
+        if(b>c)
+        max=b;
+        else max=c;
+    }
+    if(c>max)
+    max=c;
+    
+    cout<<"\n maximum is: "<<max;*/
+
+
+    short int num;
+    cout<<"enter any positive integer: "<<endl;
+    cin>>num;
+    if(num%2==0)
+    cout<<"even";
+    else cout<<"odd";
+    return 0;
+}
