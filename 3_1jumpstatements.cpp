7787d0176d36edc5b298e5b58c79bc0da1f743ee
VISHESH KUMAR <58973489+Vishesh2001@users.noreply.github.com>
2022-01-17 11:48:46 +0530

    Add files via upload
    there are two jump statements in C++.
    1. break statement.
    2. continue statement.
    break- it takes the control out of the loop when the condition is met.
    continue statement skip the iteration and moves to the next iteration when the condition for the continue statement is met.

---
+++ 3_1jumpstatements.cpp
@@ -0,0 +1,30 @@
+/* jump statement 
+problem-1. print numbers from 1-100 except number divisible by 3
+        2. checking a number prime or not*/
+#include<iostream>
+using namespace std;
+int main()
+{ 
+    /*for (int i=1;i<=100;i++)
+   {
+    if(i%3==0)
+    {continue;}
+    cout<<i<<"\n";
+   }*/
+   
+   //prime number checking
+   int num,i;
+   cout<<"enter a number to check if it is prime or not: ";
+   cin>>num;
+   for( i=2;i<num;i++)
+   {
+       if(num%i==0)
+       break;
+   }
+   if(i==num)
+   {cout<<"prime number";}
+   else cout<<"not prime";
+   
+
+    return 0;
+}
