c247b1de0f5af3bcd86d909e270f14c06ec0fd26
VISHESH KUMAR <58973489+Vishesh2001@users.noreply.github.com>
2022-01-18 09:32:25 +0530

    Add files via upload
    this is a program with no logic no input no output...ha haa haa...
    actually it contains theory about the functions., enjoy.

---
+++ 6_1functions.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    A function is a set of statements that take inputs, do some specific computation and produces output.
+
+    The idea is to put some commonly or repeatedly done task together and make a function so that instead of writing the same code again and again for different inputs, we can call the function.
+
+    The general form of a function is:
+
+    return_type function_name([ arg1_type arg1_name, ... ])
+     { code }    
+
+
+    concept of call stack.
+    a stack of functions is mainted in the memory such that.
+    when a fuction is called its stack frame is pushed on the top of the call stack.
+    and when the function returns, its stackframe is popped out of the cll stack.
+
+    stack frame is the memory int the form stack which has the function code, local variables, return values.,etc.
+    */
+
+
+    return 0;
+}
